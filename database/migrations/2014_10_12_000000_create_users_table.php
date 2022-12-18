da35392cc80c17acfadfeadf93d81a6eed82e006
aripbudiman <muhamadarifbudiman22@gmail.com>
2022-12-18 11:20:41 +0700

    tempalte 1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role',['administrator','customer'])->default('customer');
+            $table->random_int(1,0);
             $table->rememberToken();
             $table->timestamps();
         });
