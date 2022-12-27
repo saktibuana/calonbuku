290a986e488f76f48b672205654bb2c212757397
garil <garilraihan@gmail.com>
2022-12-27 20:19:33 +0700

    img-shop-popup

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function on() {
+    var menunBar = document.getElementById("menuBar");
+  
+    var overlay = document.getElementById("overlay");
+  
+    menunBar.classList.toggle("menuBarStyle");
+    overlay.classList.toggle("overlayStyle");
+  }
